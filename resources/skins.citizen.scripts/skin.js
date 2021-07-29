6b113010e96c3c108a1eaeca0dff17b4deb752a6
Simon Laroche <hi@simon.lc>
2021-07-29 00:22:56 -0400

    Remove console warning

--- resources/skins.citizen.scripts/skin.js
+++ resources/skins.citizen.scripts/skin.js
@@ -122,10 +122,6 @@ function main( window ) {
 	}, false );
 
 	mw.loader.load( 'skins.citizen.preferences' );
-
-	// Soft depreciation message for CSS var
-	// TODO: Remove when hard depreciated
-	mw.log.warn( 'CITIZEN: --background-color-dp-XX is depreciated; use --color-surface-X instead.' );
 }
 
 main( window );
